e29628329a9bc5483948ef2b6dcf31b6711b69f6
HolaMundo42 <77760629+HolaMundo42@users.noreply.github.com>
2022-08-18 10:32:39 -0300

    rework

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function sendMail(mail) {
+    $.ajax({
+      type: 'POST',
+      url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+      data: {
+        'key': 'YOUR API KEY HERE',
+        'message': {
+          'from_email': 'iam.wheelia@gmail.com',
+          'to': [
+              {
+                'email': mail,
+                'type': 'to'
+              }
+            ],
+          'autotext': 'true',
+          'subject': 'Welcome to Wheel-IA!',
+          'html': 'YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!'
+        }
+      }
+     }).done(function(response) {
+       console.log(response);
+     });
+}
